52cada99f94a166796c1a9a92e74c5d290fd48dc
Kaden Cartwright <kaden@example.com>
2026-05-23 21:56:25 -0500

    Make nh shortcut host aware

--- home/shell.nix
+++ home/shell.nix
@@ -1,3 +1,8 @@
+{ osConfig, ... }:
+
+let
+  host = osConfig.networking.hostName;
+in
 {
   programs.zsh = {
     enable = true;
@@ -5,14 +10,8 @@
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
     initContent = ''
-      nhz() { nh os switch github:kadencartwright/nix-install#Z16 -- --refresh "$@"; }
-      nhz16() { nh os switch github:kadencartwright/nix-install#Z16 -- --refresh "$@"; }
-      nhx() { nh os switch github:kadencartwright/nix-install#X1C -- --refresh "$@"; }
-      nhx1c() { nh os switch github:kadencartwright/nix-install#X1C -- --refresh "$@"; }
-      nhm() { nh os switch github:kadencartwright/nix-install#MINI -- --refresh "$@"; }
-      nhmini() { nh os switch github:kadencartwright/nix-install#MINI -- --refresh "$@"; }
-      nhp() { nh os switch github:kadencartwright/nix-install#pi5 -- --refresh "$@"; }
-      nhpi5() { nh os switch github:kadencartwright/nix-install#pi5 -- --refresh "$@"; }
+      nhos() { nh os switch github:kadencartwright/nix-install#${host} -- --refresh "$@"; }
+      nhr() { nhos "$@"; }
     '';
   };
 
