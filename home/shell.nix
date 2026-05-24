eb560b1362fc6991a805d416fb4ba5c0c7aa4ff9
Kaden Cartwright <kaden@example.com>
2026-05-23 21:55:02 -0500

    Add nh host switch shortcuts

--- home/shell.nix
+++ home/shell.nix
@@ -4,6 +4,16 @@
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
+    initContent = ''
+      nhz() { nh os switch github:kadencartwright/nix-install#Z16 -- --refresh "$@"; }
+      nhz16() { nh os switch github:kadencartwright/nix-install#Z16 -- --refresh "$@"; }
+      nhx() { nh os switch github:kadencartwright/nix-install#X1C -- --refresh "$@"; }
+      nhx1c() { nh os switch github:kadencartwright/nix-install#X1C -- --refresh "$@"; }
+      nhm() { nh os switch github:kadencartwright/nix-install#MINI -- --refresh "$@"; }
+      nhmini() { nh os switch github:kadencartwright/nix-install#MINI -- --refresh "$@"; }
+      nhp() { nh os switch github:kadencartwright/nix-install#pi5 -- --refresh "$@"; }
+      nhpi5() { nh os switch github:kadencartwright/nix-install#pi5 -- --refresh "$@"; }
+    '';
   };
 
   programs.starship.enable = true;
