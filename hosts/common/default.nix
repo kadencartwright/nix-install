1d553dbfeef073dd87524efcc065a9517738c13b
Kaden Cartwright <kaden@example.com>
2026-05-23 21:38:23 -0500

    Back up existing Home Manager files

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -20,6 +20,7 @@
 
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
+  home-manager.backupFileExtension = "hm-backup";
   home-manager.extraSpecialArgs = {
     inherit inputs;
   };
