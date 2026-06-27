424f7fee7e0b24404ae150cb7154fdd0ac1ea17f
Kaden Cartwright <kaden@example.com>
2026-06-27 14:40:55 -0500

    set default http handler

--- home/desktop.nix
+++ home/desktop.nix
@@ -1,3 +1,14 @@
 {
   xdg.enable = true;
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "application/pdf" = "chromium-browser.desktop";
+      "application/xhtml+xml" = "chromium-browser.desktop";
+      "text/html" = "chromium-browser.desktop";
+      "x-scheme-handler/http" = "chromium-browser.desktop";
+      "x-scheme-handler/https" = "chromium-browser.desktop";
+    };
+  };
 }
